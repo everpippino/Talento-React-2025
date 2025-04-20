1e472ed8cdd3aa69ccb9c20c05dd0e4d1d835fa1
Ever Pippino <everpippino@gmail.com>
2025-04-20 10:59:22 -0300

    saque comentarios

--- clase 3/Ejercicios/integrador/src/components/TarjetaProyecto.jsx
+++ clase 3/Ejercicios/integrador/src/components/TarjetaProyecto.jsx
@@ -4,9 +4,9 @@ function TarjetaProyecto({titulo,descripcion,botonTexto}){
     const handleClick = () => {
         alert(`Explorando: ${titulo}`);
       };
-    return (
-        <div className="card h-100">//Añadí h-100 al div con la clase card para que todas las tarjetas en la misma fila tengan la misma altura
-            <div className="card-body d-flex flex-column justify-content-between">//Dentro de card-body, utilicé d-flex flex-column justify-content-between para alinear el contenido verticalmente, distribuyendo el espacio entre el título, la descripción y el botón.
+    return (        
+        <div className="card h-100">            
+            <div className="card-body d-flex flex-column justify-content-between">
                 <h5 className="card-title">{titulo}</h5>
                 <p className="card-text">{descripcion}</p>
                 <button className="btn btn-primary" onClick={handleClick}>
