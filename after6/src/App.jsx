571189d3ec1bd7fc21fedcb965f78eef919d714d
Ever Pippino <everpippino@gmail.com>
2025-05-16 08:01:47 -0300

    uso de useEffect con api de Rick and Morty

---
+++ after6/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState,useEffect } from 'react'
+
+function App() {
+  const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response=> response.json())
+    .then(data=>{
+      setCharacters(data.results)
+      setLoading(false)
+    })
+    .catch(error=>console.error('Error al cargar los personajes',error))
+  })
+
+  return (
+  <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Personajes de Rick and Morty</h1>
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
+        <div>
+          {characters.map(character => (
+            <div key={character.id} style={{ margin: '1rem', display: 'inline-block' }}>
+              <img src={character.image} alt={character.name} style={{ width: '150px', height: '150px', borderRadius: '50%' }} />
+              <h3>{character.name}</h3>
+            </div>
+          ))}
+        </div>
+          )}
+  </div>
+  )
+}
+
+export default App
